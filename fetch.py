9780250bf4b6358adbcd39852bb37328d11a58bc
jsf80238 <jsf80238@gmail.com>
2023-04-01 18:17:54 -0600

    Release 0.6.0 (#12)
    In preparation for running via Google Cloud Run:
    * Get the REST API token from Secrets Manager.
    * Run as a Flask app.

--- fetch.py
+++ fetch.py
@@ -8,24 +8,23 @@ import logging
 import os
 import pathlib
 import requests
-from time import sleep
+import time
 # Imports above are standard Python
 # Imports below are 3rd-party
 from retry import retry
 from google.cloud import pubsub_v1
 import google.cloud.logging
 from google.cloud.logging.handlers import CloudLoggingHandler
+from google.cloud import secretmanager
+from flask import Flask
 
 BASE_URL = "https://www.ncei.noaa.gov/cdo-web/api/v2"
-TOKEN_FILE = "NCDC_CDO_web_services_token"
+TOKEN_SECRET_NAME = "ncdc_cdo_web_services_token"
 DATA_SET_ID = "GHCND"  # daily summaries
 COLORADO = "FIPS:08"
 START_DATE = "startdate"
 END_DATE = "enddate"
 STAMP_FORMAT = "%Y-%m-%d"
-HEADER_DICT = {
-    "token": open(TOKEN_FILE).read().strip(),
-}
 PROJECT_ID = "verdant-bond-262820"
 TOPIC = "weather"
 PUBSUB_TOPIC_NAME = f"projects/{PROJECT_ID}/topics/{TOPIC}"
@@ -49,13 +48,16 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(gcloud_logging_handler)
 logger.addHandler(stream_handler)
 
-# Count number of API calls, NOAA has a daily limit
-call_count = 0
+# Get our NOAA API token for the API call header
+client = secretmanager.SecretManagerServiceClient()
+resource_name = f"projects/{PROJECT_ID}/secrets/{TOKEN_SECRET_NAME}/versions/latest"
+token = client.access_secret_version(name=resource_name).payload.data.decode("UTF-8")
+header_dict = {"token": token}
 
 
 @retry(tries=3, delay=2, backoff=2, logger=logger)
 def get(url,
-        headers=HEADER_DICT,
+        headers=header_dict,
         params=None,
         timeout=10,  # seconds
         ) -> list[dict]:
@@ -63,6 +65,7 @@ def get(url,
     :param url: NOAA URL
     :param headers: authentication
     :param params: dictionary telling NOAA what data we want
+    :param timeout: give up after this many seconds
     :return: the data from the API call
     """
     METADATA = "metadata"
@@ -90,61 +93,84 @@ def get(url,
 
 
 # Set-up complete, program starts here
-# Create publisher
-publisher = pubsub_v1.PublisherClient()
+# Count number of API calls, NOAA has a daily limit
+call_count, error_count = 0, 0
+# Allow invocation via HTTP
+app = Flask(__name__)
 
-# Get the different measurement types we might see (NOAA calls them datatypes)
-url = f"{BASE_URL}/datatypes"
-param_dict = {
-    "locationid": COLORADO,
-    END_DATE: datetime(2000, 1, 1).strftime(STAMP_FORMAT),
-}
-logger.info(f"Getting {url} with parameters {param_dict} ...")
-data_list = get(url, params=param_dict)
-for datatype in data_list:
-    logger.debug(datatype)
-    payload = json.dumps(datatype, sort_keys=True, indent=2).encode()
-    future = publisher.publish(PUBSUB_TOPIC_NAME, payload, record_type="datatype")
-    logger.info(f"Successfully posted message_id: {future.result()}.")
 
-# Iterate over each station in Colorado with data between January 1, 2000 and today
-# The enddate and startdate parameters below look reversed but they are correct,
-# see https://www.ncdc.noaa.gov/cdo-web/webservices/v2#stations.
-url = f"{BASE_URL}/stations"
-param_dict = {
-    "locationid": COLORADO,
-    END_DATE: datetime(2000, 1, 1).strftime(STAMP_FORMAT),
-    START_DATE: (datetime.today() - timedelta(7)).strftime(STAMP_FORMAT),
-}
-logger.info(f"Getting {url} with parameters {param_dict} ...")
-data_list = get(url, params=param_dict)
+@app.route("/")
+def run():
+    # Note time
+    start_time = time.monotonic()
+    # Create publisher
+    publisher = pubsub_v1.PublisherClient()
 
-for station in data_list:
-    payload = json.dumps(station, sort_keys=True, indent=2).encode()
-    future = publisher.publish(PUBSUB_TOPIC_NAME, payload, record_type="station")
-    logger.info(f"Successfully posted message_id: {future.result()}.")
-    logger.info(f"Examining station: {station} ...")
-    url = f"{BASE_URL}/data"
+    # Get the different measurement types we might see (NOAA calls them datatypes)
+    url = f"{BASE_URL}/datatypes"
     param_dict = {
-        "datasetid": DATA_SET_ID,
-        "stationid": station["id"],
-        "units": "metric",
-        START_DATE: (datetime.today() - timedelta(7)).strftime(STAMP_FORMAT),
-        END_DATE: (datetime.today()).strftime(STAMP_FORMAT),
+        "locationid": COLORADO,
+        END_DATE: datetime(2000, 1, 1).strftime(STAMP_FORMAT),
     }
     logger.info(f"Getting {url} with parameters {param_dict} ...")
-    try:
-        data_list = get(url, params=param_dict)
-    except Exception as e:
-        # It's okay if we cannot get a particular station's measurements, just log it
-        logger.error(f"Skipping station '{station['id']}', error message was: {e}.")
-        continue
-    # Iterate over the measured data and write to the database
-    for measurement in data_list:
-        logger.info(measurement)
-        # Publish
-        payload = json.dumps(measurement, sort_keys=True, indent=2).encode()
-        future = publisher.publish(PUBSUB_TOPIC_NAME, payload, record_type="measurement")
+    data_list = get(url, params=param_dict)
+    for datatype in data_list:
+        logger.debug(datatype)
+        payload = json.dumps(datatype, sort_keys=True, indent=2).encode()
+        future = publisher.publish(PUBSUB_TOPIC_NAME, payload, record_type="datatype")
         logger.info(f"Successfully posted message_id: {future.result()}.")
 
-logger.info(f"Finished. Made {call_count} API calls.")
+    # Iterate over each station in Colorado with data between January 1, 2000 and today
+    # The enddate and startdate parameters below look reversed but they are correct,
+    # see https://www.ncdc.noaa.gov/cdo-web/webservices/v2#stations.
+    url = f"{BASE_URL}/stations"
+    param_dict = {
+        "locationid": COLORADO,
+        END_DATE: datetime(2000, 1, 1).strftime(STAMP_FORMAT),
+        START_DATE: (datetime.today() - timedelta(7)).strftime(STAMP_FORMAT),
+    }
+    logger.info(f"Getting {url} with parameters {param_dict} ...")
+    data_list = get(url, params=param_dict)
+
+    for station in data_list:
+        payload = json.dumps(station, sort_keys=True, indent=2).encode()
+        future = publisher.publish(PUBSUB_TOPIC_NAME, payload, record_type="station")
+        logger.info(f"Successfully posted message_id: {future.result()}.")
+        logger.info(f"Examining station: {station} ...")
+        url = f"{BASE_URL}/data"
+        param_dict = {
+            "datasetid": DATA_SET_ID,
+            "stationid": station["id"],
+            "units": "metric",
+            START_DATE: (datetime.today() - timedelta(7)).strftime(STAMP_FORMAT),
+            END_DATE: (datetime.today()).strftime(STAMP_FORMAT),
+        }
+        logger.info(f"Getting {url} with parameters {param_dict} ...")
+        try:
+            data_list = get(url, params=param_dict)
+        except Exception as e:
+            # It's okay if we cannot get a particular station's measurements, just log it
+            logger.error(f"Skipping station '{station['id']}', error message was: {e}.")
+            global error_count
+            error_count += 1
+            continue
+        # Iterate over the measured data and write to the database
+        for measurement in data_list:
+            logger.info(measurement)
+            # Publish
+            payload = json.dumps(measurement, sort_keys=True, indent=2).encode()
+            future = publisher.publish(PUBSUB_TOPIC_NAME, payload, record_type="measurement")
+            logger.info(f"Successfully posted message_id: {future.result()}.")
+
+    duration = int(time.monotonic() - start_time)
+    logger.info(f"Finished. Made {call_count} API calls.")
+    return {
+        "api_call_count": call_count,
+        "error_count": error_count,
+        "duration_in_seconds": duration,
+    }
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
